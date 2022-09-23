7b46f4822f0c79c315c2ea9d86e9f4d3199bd980
Abdu Sallouh <31540503+abduABS@users.noreply.github.com>
2022-09-23 19:53:23 +0400

    added more items

--- Code.js
+++ Code.js
@@ -6,6 +6,37 @@ document.addEventListener("DOMContentLoaded", function(event) {
     function incrsize(){
         //increase size of all fonts on page
         document.body.style.fontSize = "xx-large";
+        
+        var h1Elements = document.getElementsByTagName("h1");
+        for(var i = 0; i < h1Elements.length; i++) {
+            h1Elements[i].style.fontSize = "120px";
+        }
+
+        var h2Elements = document.getElementsByTagName("h2");
+        for(var i = 0; i < h2Elements.length; i++) {
+            h2Elements[i].style.fontSize = "50px";
+        }
+
+        var h3Elements = document.getElementsByTagName("h3");
+        for(var i = 0; i < h3Elements.length; i++) {
+            h3Elements[i].style.fontSize = "xx-large";
+        }
+
+        var pElements = document.getElementsByTagName("p");
+        for(var i = 0; i < pElements.length; i++) {
+            pElements[i].style.fontSize = "40px";
+        }
+
+        var ulElements = document.getElementsByTagName("ul");
+        for(var i = 0; i < ulElements.length; i++) {
+            ulElements[i].style.fontSize = "32px";
+        }
+
+        var olElements = document.getElementsByTagName("ol");
+        for(var i = 0; i < olElements.length; i++) {
+            olElements[i].style.fontSize = "32px";
+        }
+
     }
 
     document.getElementById('rstsize').addEventListener("click",rstsize);
