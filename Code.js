bdddbe826600f53756b5cabf7700932263955921
Abdu Sallouh <31540503+abduABS@users.noreply.github.com>
2022-09-23 20:03:37 +0400

    Updated Code.js

--- Code.js
+++ Code.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     function incrsize(){
         //increase size of all fonts on page
         document.body.style.fontSize = "xx-large";
-        
+
         var h1Elements = document.getElementsByTagName("h1");
         for(var i = 0; i < h1Elements.length; i++) {
             h1Elements[i].style.fontSize = "120px";
@@ -37,18 +37,103 @@ document.addEventListener("DOMContentLoaded", function(event) {
             olElements[i].style.fontSize = "32px";
         }
 
+        var labelElements = document.getElementsByTagName("label");
+        for(var i = 0; i < labelElements.length; i++) {
+            labelElements[i].style.fontSize = "x-large";
+        }
+
+        var aElements = document.getElementsByTagName("a");
+        for(var i = 0; i < aElements.length; i++) {
+            aElements[i].style.fontSize = "x-large";
+        }
+
     }
 
     document.getElementById('rstsize').addEventListener("click",rstsize);
     function rstsize(){
         //increase size of all fonts on page
         document.body.style.fontSize = "medium";
+
+        var h1Elements = document.getElementsByTagName("h1");
+        for(var i = 0; i < h1Elements.length; i++) {
+            h1Elements[i].style.fontSize = "60px";
+        }
+
+        var h2Elements = document.getElementsByTagName("h2");
+        for(var i = 0; i < h2Elements.length; i++) {
+            h2Elements[i].style.fontSize = "25px";
+        }
+
+        var h3Elements = document.getElementsByTagName("h3");
+        for(var i = 0; i < h3Elements.length; i++) {
+            h3Elements[i].style.fontSize = "x-large";
+        }
+
+        var pElements = document.getElementsByTagName("p");
+        for(var i = 0; i < pElements.length; i++) {
+            pElements[i].style.fontSize = "20px";
+        }
+
+        var ulElements = document.getElementsByTagName("ul");
+        for(var i = 0; i < ulElements.length; i++) {
+            ulElements[i].style.fontSize = "16px";
+        }
+
+        var olElements = document.getElementsByTagName("ol");
+        for(var i = 0; i < olElements.length; i++) {
+            olElements[i].style.fontSize = "16px";
+        }
+
+        var labelElements = document.getElementsByTagName("label");
+        for(var i = 0; i < labelElements.length; i++) {
+            labelElements[i].style.fontSize = "medium";
+        }
+
+        var aElements = document.getElementsByTagName("a");
+        for(var i = 0; i < aElements.length; i++) {
+            aElements[i].style.fontSize = "medium";
+        }
     }
 
     document.getElementById('chtype').addEventListener("click",comic);
     function comic(){
         //increase size of all fonts on page
         document.body.style.fontFamily = "Comic Sans MS"
+
+        var h1Elements = document.getElementsByTagName("h1");
+        for(var i = 0; i < h1Elements.length; i++) {
+            h1Elements[i].style.fontFamily = "Comic Sans MS";
+        }
+
+        var h2Elements = document.getElementsByTagName("h2");
+        for(var i = 0; i < h2Elements.length; i++) {
+            h2Elements[i].style.fontFamily = "Comic Sans MS";
+        }
+
+        var h3Elements = document.getElementsByTagName("h3");
+        for(var i = 0; i < h3Elements.length; i++) {
+            h3Elements[i].style.fontFamily = "Comic Sans MS";
+        }
+
+        var pElements = document.getElementsByTagName("p");
+        for(var i = 0; i < pElements.length; i++) {
+            pElements[i].style.fontFamily = "Comic Sans MS";
+        }
+
+        var ulElements = document.getElementsByTagName("ul");
+        for(var i = 0; i < ulElements.length; i++) {
+            ulElements[i].style.fontFamily = "Comic Sans MS";
+        }
+
+        var olElements = document.getElementsByTagName("ol");
+        for(var i = 0; i < olElements.length; i++) {
+            olElements[i].style.fontFamily = "Comic Sans MS";
+        }
+
+        var aElements = document.getElementsByTagName("a");
+        for(var i = 0; i < aElements.length; i++) {
+            aElements[i].style.fontFamily = "Comic Sans MS";
+        }
     }
 
 
